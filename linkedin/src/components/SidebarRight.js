07fbc220f6a5f7795b799d7ceec70175775a06f6
Aditya Uday Ubale <51261116+aditya25022001@users.noreply.github.com>
2021-04-09 11:12:57 +0530

    sidebar

--- linkedin/src/components/SidebarRight.js
+++ linkedin/src/components/SidebarRight.js
@@ -5,7 +5,7 @@ import LaunchTwoToneIcon from '@material-ui/icons/LaunchTwoTone';
 import ArrowForwardTwoToneIcon from '@material-ui/icons/ArrowForwardTwoTone';
 import Tooltip from '@material-ui/core/Tooltip';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
-import { ListGroup, ListGroupItem } from 'react-bootstrap' 
+import { Image, ListGroup } from 'react-bootstrap' 
 
 export const SidebarRight = () => {
     
@@ -16,6 +16,17 @@ export const SidebarRight = () => {
                         <div style={{ fontWeight:500, fontSize:14 }}>{heading}</div>
                         <div className='align-items-center' style={{ fontSize:12 }} className='text-muted font-weight-light'>{when}{" "}ago <FiberManualRecordIcon style={{ fontSize:6, color:'black' }} />{" "}{readers}{" "}readers</div>
                     </div>
+                    </ListGroup.Item>
+    }
+
+    const setCourseItem = (index,heading,description) => {
+        return <ListGroup.Item id='sidebar__news' className='px-3 pt-0 border-0 d-flex flex-direction-row rounded' style={{alignItems:'baseline' }}>
+                    <div className='mr-2' style={{ fontWeight:500, fontSize:15 }}>{index}.</div>
+                    <div>
+                        <div style={{ fontWeight:500, fontSize:14 }}>{heading.slice(0,40)}...</div>
+                        <div className='align-items-center' style={{ fontSize:12 }} className='text-muted font-weight-light'>{description.slice(0,40)}...</div>
+                    </div>
+                    <div className='ml-1 font-weight-bold' id='launch_another_news'><LaunchTwoToneIcon style={{ fontSize:15}}/></div>
                     </ListGroup.Item>
     }
     
@@ -41,12 +52,27 @@ export const SidebarRight = () => {
                     {setNewsItem('Covid surge kills shoppers\'\ appetite','10d','1,580')}
                 </ListGroup>
             </ListGroup.Item>
-            <ListGroup.Item className='border-top rounded'>
-                <ListGroup className='border-0' >
-                    <ListGroup.Item className='border-0'>
-
-                    </ListGroup.Item>
+            <ListGroup.Item className='border-top rounded px-0 mb-2'>
+                <ListGroup className='border-0 py-2' >
+                    <div className='px-3 d-flex flex-direction-row pb-2' style={{ justifyContent:'space-between' }}>
+                        <div>Today's most viewed courses</div>
+                        <Tooltip placement="right" title="These are the day’s most watched courses on LinkedIn Learning. Learn more about how they’re selected.">
+                            <InfoIcon style={{ fontSize:18 }} />
+                        </Tooltip>
+                    </div>
+                    {setCourseItem(1,'The Six Morning Habits of High Performance','Pete Mockaitis | How to be Awesome at Your')}
+                    {setCourseItem(2,'Body Language for Authentic Leaders','Erica Dhawan')}
+                    {setCourseItem(3,'Speaking Confidently and Effectively','Pete Mockaitis | How to be Awesome at Your')}
+                    <div className="d-flex flex-direction-row align-items-center px-3">
+                        <div className='text-muted' style={{ fontWeight:500, fontSize:14 }}>Show more on LinkedIn learning</div>
+                        <div className='text-muted' style={{ fontWeight:500 }}>
+                            <ArrowForwardTwoToneIcon style={{ fontSize:15}}/>
+                        </div>
+                    </div>
                 </ListGroup>
+            </ListGroup.Item>
+            <ListGroup.Item className='border-top rounded py-0 px-1'>
+                <Image src='https://static-exp1.licdn.com/scds/common/u/images/promo/ads/li_evergreen_jobs_ad_300x250_v1.jpg' style={{ width:'22.3vw' }}/>
             </ListGroup.Item>
         </ListGroup>
     )
